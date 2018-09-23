9b2c08e1023ae2a3bfddf4d43d564774a96bf27a
Graeme Hosford <graeme.hosford@mycit.ie>
2018-09-23 11:41:07 +0100

    Fixed button layout for navigation through fragments

--- app/src/main/java/group/project/transportai/BookingActivity.java
+++ app/src/main/java/group/project/transportai/BookingActivity.java
@@ -11,12 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class BookingActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     Fragment locationFragment, reviewFragment, travelPointsFragment;
     FragmentManager fragmentManager;
+
+    Button bPrevious, bNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,14 @@ public class BookingActivity extends AppCompatActivity
         travelPointsFragment = new TravelPointsFragment();
 
         fragmentManager.beginTransaction().replace(R.id.flBookingScreenArea, locationFragment).commit();
+
+        bPrevious = findViewById(R.id.bPrevious);
+        bPrevious.setOnClickListener(this);
+        bPrevious.setVisibility(View.INVISIBLE);
+        bPrevious.setClickable(false);
+
+        bNext = findViewById(R.id.bNext);
+        bNext.setOnClickListener(this);
     }
 
     @Override
@@ -85,12 +97,30 @@ public class BookingActivity extends AppCompatActivity
         switch(item.getItemId()) {
             case R.id.nav_bookRide:
                 fragment = locationFragment;
+
+                bPrevious.setVisibility(View.INVISIBLE);
+                bPrevious.setClickable(false);
+
+                bNext.setVisibility(View.VISIBLE);
+                bNext.setClickable(true);
                 break;
             case R.id.nav_myReviews:
                 fragment = reviewFragment;
+
+                bPrevious.setVisibility(View.INVISIBLE);
+                bPrevious.setClickable(false);
+
+                bNext.setVisibility(View.INVISIBLE);
+                bNext.setClickable(false);
                 break;
             case R.id.nav_travelPoints:
                 fragment = travelPointsFragment;
+
+                bPrevious.setVisibility(View.INVISIBLE);
+                bPrevious.setClickable(false);
+
+                bNext.setVisibility(View.INVISIBLE);
+                bNext.setClickable(false);
                 break;
         }
 
@@ -103,5 +133,17 @@ public class BookingActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.bPrevious:
+                // TODO Move to previous fragment in booking process, make invisible and unclickable if going back to LocationFragment
+                break;
+            case R.id.bNext:
+                // TODO Move forward to next fragment, make bPrevious visible and clickable to go back
+                break;
+        }
     }
 }
