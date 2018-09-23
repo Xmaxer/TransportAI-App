7ad1756c245576fa6a85b360faefb3744383d3ad
Graeme Hosford <graeme.hosford@mycit.ie>
2018-09-23 10:47:02 +0100

    Fixed bug in LocationFragment  which caused app to crash when attempting to reload the fragment.

--- app/src/main/java/group/project/transportai/LocationFragment.java
+++ app/src/main/java/group/project/transportai/LocationFragment.java
@@ -1,12 +1,17 @@
 package group.project.transportai;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
+
 public class LocationFragment extends Fragment {
+
+    SupportPlaceAutocompleteFragment fromSearch, toSearch;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -15,5 +20,14 @@ public class LocationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_location, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        fromSearch = (SupportPlaceAutocompleteFragment) getActivity()
+                .getSupportFragmentManager().findFragmentById(R.id.placeAutoCompleteFrom);
+
+        toSearch = (SupportPlaceAutocompleteFragment) getActivity()
+                .getSupportFragmentManager().findFragmentById(R.id.placeAutoCompleteTo);
+    }
 }
