84b0fdaaaf92bc72ff44a33ee585ad6055d6109c
Graeme Hosford <graeme.hosford@mycit.ie>
2018-10-10 12:46:52 +0100

    Setup up retrieval of travel points from FireStore

--- app/src/main/java/group/project/transportai/TravelPointsFragment.java
+++ app/src/main/java/group/project/transportai/TravelPointsFragment.java
@@ -1,5 +1,6 @@
 package group.project.transportai;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,6 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class TravelPointsFragment extends Fragment {
 
@@ -23,6 +32,24 @@ public class TravelPointsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         tvTotalPoints = view.findViewById(R.id.tvTotalTravelPoints);
-        tvTotalPoints.setText("2367");
+        getTravelPoints();
+    }
+
+    private void getTravelPoints() {
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore travelPointsDB = FirebaseFirestore.getInstance();
+
+        travelPointsDB.collection("points").whereEqualTo("userID", currentUser.getUid()).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot doc : task.getResult()) {
+                                tvTotalPoints.setText(doc.get("points").toString());
+                            }
+                        }
+                    }
+                });
     }
 }
