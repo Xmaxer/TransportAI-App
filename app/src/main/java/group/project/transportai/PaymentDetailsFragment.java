d82cf0ef408d9c5fe36ecc7bdaba63a1ffb7319c
Graeme Hosford <graeme.hosford@mycit.ie>
2018-10-16 12:20:27 +0100

    Added PaymentDetailsFragment.java to show user the details of their car ride in order for them to make the final payment

---
+++ app/src/main/java/group/project/transportai/PaymentDetailsFragment.java
@@ -0,0 +1,149 @@
+package group.project.transportai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.braintreepayments.api.dropin.DropInRequest;
+import com.braintreepayments.api.dropin.DropInResult;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.app.Activity.RESULT_OK;
+
+public class PaymentDetailsFragment extends Fragment implements View.OnClickListener {
+
+    private String origin, destination, distance, cost;
+    private static int PAYPAL_REQUEST_CODE = 2120;
+
+    private static final String API_CHECKOUT = "https://ardra.herokuapp.com/braintree/checkout";
+
+    private HashMap<String, String> paramsHashmap;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        Bundle args = this.getArguments();
+
+        origin = args.getString("Origin");
+        destination = args.getString("Destination");
+        distance = args.getString("Distance");
+        cost = args.getString("Cost");
+
+        return inflater.inflate(R.layout.fragment_payment_details_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView originText = view.findViewById(R.id.tvOriginData);
+        originText.setText(origin);
+
+        TextView destText = view.findViewById(R.id.tvDestinationData);
+        destText.setText(destination);
+
+        TextView distanceText = view.findViewById(R.id.tvDistanceData);
+        distanceText.setText(distance);
+
+        TextView costText = view.findViewById(R.id.tvCostData);
+        costText.setText(cost);
+
+        Button payButton = view.findViewById(R.id.bPay);
+        payButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        makePayment();
+    }
+
+    private void makePayment() {
+        DropInRequest dropIn = new DropInRequest().clientToken("sandbox_b8hbk65s_s2w84n9y8wd4dkpm");
+        startActivityForResult(dropIn.getIntent(getActivity()), PAYPAL_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PAYPAL_REQUEST_CODE) {
+
+            if (resultCode == RESULT_OK) {
+                DropInResult dropInResult = data.getParcelableExtra(DropInResult.EXTRA_DROP_IN_RESULT);
+                PaymentMethodNonce paymentNonce = dropInResult.getPaymentMethodNonce();
+
+                String strNonce = paymentNonce.getNonce();
+
+                paramsHashmap = new HashMap<>();
+                paramsHashmap.put("amount", cost);
+                paramsHashmap.put("payment_method_nonce", strNonce);
+
+                sendPayment();
+            }
+        }
+    }
+
+    private void sendPayment() {
+
+        RequestQueue requestQ = Volley.newRequestQueue(getActivity());
+
+        StringRequest strRequest = new StringRequest(Request.Method.POST, API_CHECKOUT,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.contains("Successful")) {
+                            Toast.makeText(getActivity(), "Payment Completed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Toast.makeText(getActivity(), "Payment Failed", Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+
+                Map<String, String> params = new HashMap<>();
+
+                for (String key : paramsHashmap.keySet()) {
+                    params.put(key, paramsHashmap.get(key));
+                }
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headerParams = new HashMap<>();
+                headerParams.put("Content-Type", "application/x-www-form-urlencoded");
+                return headerParams;
+            }
+        };
+
+        requestQ.add(strRequest);
+    }
+}
