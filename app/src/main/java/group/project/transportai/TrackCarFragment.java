123bb6fd09ce2d8ae57fc2446c894a8fbf0ff1af
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-10-26 16:04:37 +0100

    TrackCarFragment.java will eventually track movement of car on map, fragment_track_car.xml contains the map which will be used

--- app/src/main/java/group/project/transportai/TrackCarFragment.java
+++ app/src/main/java/group/project/transportai/TrackCarFragment.java
@@ -1,4 +1,35 @@
 package group.project.transportai;
 
-public class TrackCarFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class TrackCarFragment extends Fragment implements OnMapReadyCallback {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_track_car, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment trackCarMap = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapTrackCar);
+        trackCarMap.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMinZoomPreference(2.0f);
+    }
 }
