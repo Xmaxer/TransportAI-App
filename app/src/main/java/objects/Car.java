13462aa69d211b9819d352d93643c1de9d1c6806
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-10-26 15:59:38 +0100

    Updated fields to  reflect changes in database structure

--- app/src/main/java/objects/Car.java
+++ app/src/main/java/objects/Car.java
@@ -2,18 +2,34 @@ package objects;
 
 public class Car {
 
-    private String carModel;
+    private String make, model, regNo;
+    private int status;
 
-    public Car(String carModel) {
-        this.carModel = carModel;
+    public Car(String make, String model, String regNo, int status) {
+        this.make = make;
+        this.model = model;
+        this.status = status;
+        this.regNo = regNo;
+    }
+
+    public String getMake() {
+        return make;
     }
 
     public String getCarModel() {
-        return carModel;
+        return model;
+    }
+
+    public String getRegNumber() {
+        return regNo;
+    }
+
+    public int getStatus() {
+        return status;
     }
 
     @Override
     public String toString(){
-        return carModel;
+        return make + " " + model;
     }
 }
