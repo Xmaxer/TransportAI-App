75f89d26c66264bc8be48f0f6a1db863d6fbf065
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-10-28 20:52:46 +0000

    Basic functionality of car tracking now works. Only works for single car in database and does not currently check if user has even ordered a car

--- app/src/main/java/group/project/transportai/TrackCarFragment.java
+++ app/src/main/java/group/project/transportai/TrackCarFragment.java
@@ -1,18 +1,33 @@
 package group.project.transportai;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class TrackCarFragment extends Fragment implements OnMapReadyCallback {
+
+    private static GoogleMap map;
+    private static Marker carMarker;
 
     @Nullable
     @Override
@@ -30,6 +45,66 @@ public class TrackCarFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.setMinZoomPreference(2.0f);
+        map = googleMap;
+        map.setMinZoomPreference(2.0f);
+
+        new GetCarLocation().execute();
+    }
+
+    private static class GetCarLocation extends AsyncTask<Void, GeoPoint, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            while(true) {
+
+                FirebaseFirestore.getInstance().collection("cars").get().
+                        addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot doc = task.getResult().getDocuments().get(0);
+
+                                    GeoPoint location;
+
+                                    location = (GeoPoint) doc.get("location");
+
+                                    Log.d("Tracking", "Have Location");
+
+                                    publishProgress(location);
+
+                                }
+                            }
+                        });
+
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+
+        @Override
+        protected void onProgressUpdate(GeoPoint... values) {
+            super.onProgressUpdate(values);
+
+            GeoPoint point = values[0];
+
+            Log.d("Tracking", "Updating marker");
+
+            LatLng latLng = new LatLng(point.getLatitude(), point.getLongitude());
+
+            if(carMarker != null) {
+                carMarker.remove();
+            }
+
+            carMarker = map.addMarker(new MarkerOptions().position(latLng).title("Car Location"));
+            map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            map.animateCamera(CameraUpdateFactory.zoomTo(16.0f));
+        }
     }
 }
