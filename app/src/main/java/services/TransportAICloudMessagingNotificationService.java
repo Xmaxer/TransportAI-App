6446ac8f13731d515f6af8cf6dfece3f5f16412c
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-11-05 18:42:20 +0000

    Classs now handles receiving a notification and displaying it

--- app/src/main/java/services/TransportAICloudMessagingNotificationService.java
+++ app/src/main/java/services/TransportAICloudMessagingNotificationService.java
@@ -1,14 +1,62 @@
 package services;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
+
+import group.project.transportai.R;
 
 public class TransportAICloudMessagingNotificationService extends FirebaseMessagingService {
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+    }
 
-        
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        if(remoteMessage.getData().isEmpty()) {
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+        } else {
+            showNotification(remoteMessage.getData());
+        }
+    }
+
+    private void showNotification(Map<String, String> data) {
+        showNotification(data.get("title"), data.get("body"));
+    }
+
+    private void showNotification(String title, String body) {
+
+        NotificationManager notifManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String NOTIFICATION_CHANNEL_ID = "TransportAI";
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    "Ardra", NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription("TransportAI Notification Channel");
+
+            notifManager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSmallIcon(R.mipmap.ardra_logo_round);
+
+        notifManager.notify(23,  builder.build());
 
     }
 }
