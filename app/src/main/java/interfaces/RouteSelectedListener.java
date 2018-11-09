3041c8b43c317beb7a73a0c58bfb138b4288b051
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-11-09 14:32:20 +0000

    Modified onRouteSelected to include coordinates

--- app/src/main/java/interfaces/RouteSelectedListener.java
+++ app/src/main/java/interfaces/RouteSelectedListener.java
@@ -1,7 +1,9 @@
 package interfaces;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public interface RouteSelectedListener {
-    void onRouteSelected(String origin, String destination);
+    void onRouteSelected(String origin, String destination, LatLng originCoords, LatLng destCoords);
 
     void onDistanceCalculated(double distance);
 }
