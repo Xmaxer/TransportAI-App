2a434f16302ea69dfe4573247b0705751ff2afba
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-11-12 21:10:11 +0000

    Changed onDistanceCalculated to onJourneyCalculated and also includes time as a parameter

--- app/src/main/java/interfaces/RouteSelectedListener.java
+++ app/src/main/java/interfaces/RouteSelectedListener.java
@@ -5,5 +5,5 @@ import com.google.android.gms.maps.model.LatLng;
 public interface RouteSelectedListener {
     void onRouteSelected(String origin, String destination, LatLng originCoords, LatLng destCoords);
 
-    void onDistanceCalculated(double distance);
+    void onJourneyCalculated(double distance, int time);
 }
