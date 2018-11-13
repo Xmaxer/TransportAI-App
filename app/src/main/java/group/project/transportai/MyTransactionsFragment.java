de9546c8142aca306863a482a66430f96604f2ef
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-11-13 12:49:33 +0000

    Implemented functionality

--- app/src/main/java/group/project/transportai/MyTransactionsFragment.java
+++ app/src/main/java/group/project/transportai/MyTransactionsFragment.java
@@ -1,4 +1,47 @@
 package group.project.transportai;
 
-public class MyTransactionsFragment {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import adapters.CarSelectionListAdapter;
+import adapters.MyTransactionsAdapter;
+
+public class MyTransactionsFragment extends Fragment {
+
+    private RecyclerView rvMyTransactions;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_transactions, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvMyTransactions = view.findViewById(R.id.rvMyTransactions);
+
+        LinearLayoutManager linearLayout = new LinearLayoutManager(getContext());
+
+        rvMyTransactions.setLayoutManager(linearLayout);
+
+        DividerItemDecoration divider = new DividerItemDecoration(rvMyTransactions.getContext(), linearLayout.getOrientation());
+
+        rvMyTransactions.addItemDecoration(divider);
+
+        rvMyTransactions.setItemAnimator(new DefaultItemAnimator());
+
+        MyTransactionsAdapter adapter = new MyTransactionsAdapter();
+        rvMyTransactions.setAdapter(adapter);
+    }
 }
