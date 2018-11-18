f4a5117fb98162285c1b6db68e64a165a270435f
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-11-18 21:33:34 +0000

    Created a new object in order to store information on points a user has earned

---
+++ app/src/main/java/objects/PointsEarned.java
@@ -0,0 +1,27 @@
+package objects;
+
+public class PointsEarned {
+
+    private String date, pointsGained;
+
+    public PointsEarned(String date, String pointsGained) {
+        this.date = date;
+        this.pointsGained = pointsGained;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getPointsGained() {
+        return pointsGained;
+    }
+
+    public void setPointsGained(String pointsGained) {
+        this.pointsGained = pointsGained;
+    }
+}
