c9786bb944a3120660baaa6bc18cb9371b04c4da
Graeme Hosford - R00147327 <graeme.hosford@mycit.ie>
2018-11-19 13:19:05 +0000

    Setup recyclerview in code

--- app/src/main/java/group/project/transportai/TravelPointsFragment.java
+++ app/src/main/java/group/project/transportai/TravelPointsFragment.java
@@ -4,6 +4,10 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +19,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import adapters.TravelPointsEarnedAdapter;
 
 public class TravelPointsFragment extends Fragment {
 
@@ -29,6 +35,21 @@ public class TravelPointsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rvpointList = view.findViewById(R.id.rvpointsList);
+
+        LinearLayoutManager linearLayout = new LinearLayoutManager(getContext());
+
+        rvpointList.setLayoutManager(linearLayout);
+
+        DividerItemDecoration divider = new DividerItemDecoration(rvpointList.getContext(), linearLayout.getOrientation());
+
+        rvpointList.addItemDecoration(divider);
+
+        rvpointList.setItemAnimator(new DefaultItemAnimator());
+
+        TravelPointsEarnedAdapter pointListAdapter = new TravelPointsEarnedAdapter(getContext());
+        rvpointList.setAdapter(pointListAdapter);
 
         tvTotalPoints = view.findViewById(R.id.tvTotalTravelPoints);
         getTravelPoints();
